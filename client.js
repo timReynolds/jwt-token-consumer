19d5873f38713e5a02a68700c164c8f657c57a59
Tim Reynolds <tim@timothyreynolds.co.uk>
2014-07-08 19:07:59 +0100

    Added test code

---
+++ client.js
@@ -0,0 +1,29 @@
+var jwt = require('jsonwebtoken')
+  , fs = require('fs')
+  , request = require('request')
+  , config = require('./config.json');
+
+if(!config) {
+  console.error('Failed to load config');
+  process.exit(1);
+}
+
+var cert = fs.readFileSync(config.cert);
+
+request.post(config.provider, function (err, response, body) {
+	var data = JSON.parse(body);
+  console.log(data.token);
+  if (err && response.statusCode != 200) {
+  	console.error('Failed to get response');
+    process.exit(1);
+  }
+
+  jwt.verify(data.token, cert, function(err, decoded) {
+  	if (err) {
+  	  console.error(err);
+      process.exit(1);
+  	}
+  	console.log(decoded);
+  });
+});
+
